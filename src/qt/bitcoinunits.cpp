c48640eb48b0e2971d68eefffb3e90796b81f53d
edwinvyp <edwinvarenoyogapradana@gmail.com>
2021-04-11 13:45:47 +0000

    XEPH

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -38,9 +38,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("LTC");
-    case mBTC: return QString("mLTC");
-    case uBTC: return QString::fromUtf8("μLTC");
+    case BTC: return QString("XEPH");
+    case mBTC: return QString("mXEPH");
+    case uBTC: return QString::fromUtf8("μXEPH");
     default: return QString("???");
     }
 }
